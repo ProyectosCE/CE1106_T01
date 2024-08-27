22c60a0cfa093b133bcdb3c7554fbd5f89b40002
Alexander Montero V <alexmv223@pm.me>
2024-08-27 00:52:20 -0600

    [DOCUMENTATION] Program.asm Se documenta el archivo del programa principal, indicando el uso de los buffers y de los métodos presentes

--- ASM/program.asm
+++ ASM/program.asm
@@ -1,32 +1,44 @@
-org 100h
+[bits 16]
 
+;Seccion de datos predefinidos
 section .data
-    default_ms db 'Otras cosas', 0x0D, 0x0A, '$'  ; Mensaje por defecto
-    initial_msg db 'Bienvenido a GeometryTEC', 0x0D, 0x0A, '$'  ; Mensaje inicial que pregunta si se desea iniciar
-    askn_msg db 0x0A, 'Escoge una figura:', 0x0D, 0x0A, '$'  ; Mensaje que pide al usuario que escoja una figura
+    ;Mensaje general de pruebas
+    default_ms db 'Otras cosas', 0x0D, 0x0A, '$' 
 
-    square db 0x0A, '1. Cuadrado', 0x0D, 0x0A, '$'  ; Opción para cuadrado
-    circle db '2. Circulo', 0x0D, 0x0A, '$'  ; Opción para círculo
-    triangle db '3. Triangulo', 0x0D, 0x0A, '$'  ; Opción para triángulo
-    diamond db '4. Diamante', 0x0D, 0x0A, '$'  ; Opción para diamante
-    pentagon db '5. Pentagono', 0x0D, 0x0A, '$'  ; Opción para pentágono
-    hexagon db '6. Hexagono', 0x0D, 0x0A, '$'  ; Opción para hexágono
-    trapeze db '7. Trapecio', 0x0D, 0x0A, '$'  ; Opción para trapecio
-    parallelogram db '8. Paralelogramo', 0x0D, 0x0A, '$'  ; Opción para paralelogramo
+    ;Mensaje de bienvenida
+    initial_msg db 'Bienvenido a GeometryTEC', 0x0A, '$'
+    
+    ;Para pregunta se seleccion de figura
+    askn_msg db 0x0A, 'Escoge una figura:', '$' 
+    square db 0x0A, '1. Cuadrado', '$'  
+    circle db 0x0A, '2. Circulo', '$'  
+    triangle db 0x0A, '3. Triangulo','$'  
+    diamond db 0x0A, '4. Diamante', '$'  
+    pentagon db 0x0A, '5. Pentagono', '$'  
+    hexagon db 0x0A, '6. Hexagono', '$'  
+    trapeze db 0x0A, '7. Trapecio','$'  
+    parallelogram db 0x0A, '8. Paralelogramo',0x0A,'$' 
 
-    ask_lado db 0x0A, 'Cuanto mide el lado?', 0x0D, 0x0A, '$'  ; Pregunta cuanto mide el lado
-    result_peri db 0x0A, 'El perametro es: ', '$'  ; Mensaje para el resultado del perímetro
-    result_area db 0x0A, 'El área es: ', '$'  ; Mensaje para el resultado del área
+    ;Pregunta de lados
+    ask_lado db 0x0A, 'Cuanto mide el lado?',0x0A, '$' 
+    
+    ;Respuestas de perimetro y área
+    result_peri db 0x0A, 'El perametro es: ', '$'
+    result_area db 0x0A, 'El area es: ', '$'
 
-    repetir_msg db 0x0A, 'Por favor presione:', 0x0A, '$'  ; Mensaje que pide al usuario repetir o no
-    repsi db '1. Hacer otra operación', 0x0D, 0x0A, '$'  ; Opción para repetir la operación
-    repno db '2. Salir', 0x0D, 0x0A, '$'  ; Opción para salir
+    ; Mensaje de para consultar si quiere hacer otro calculo
+    repetir_msg db 0x0A, 'Por favor presione:', '$'
+    repsi db 0x0A,'1. Hacer otra operacion','$' 
+    repno db 0x0A,'2. Salir',0x0A,'$'
 
-    salida_msg db 0x0A, 'Gracias por usar GeometryTEC:', 0x0D, 0x0A, '$'  ; Mensaje de salida
+    ;Mensaje de agradecimiento
+    salida_msg db 0x0A, 'Gracias por usar GeometryTEC:','$'  ; Mensaje de salida
 
+;Seccion de buffer usados en todo el programa para el almacenamiento de valores y datos
 section .bss   
     ;buffer general de entrada de texto (reutilizable)
     buffer_text resb 10 ; maxima entrada del input (7 máx y 2 bytes de control)
+    
     ;buffer para datos numericos (hasta 3 para las figuras que lo ocupan)
     dato_01 resb 3 ;dato Numerico para input 1
     dato_02 resb 3 ;dato Numerico para input 2
@@ -37,26 +49,31 @@ section .bss
     operando2 resb 2
     respuesta resb 2
 
+    ;Buffer numerico de 3 bytes multiproposito
     buftemp resb 3
 
     ;Buffers para respuestas, de 5 bytes
     ; 4 bytes max para la parte entera
     ; 1 byte para la parte decimal
-    peri_r resb 5 ;para cualquier figura maximo FFFF FFFF
-    area_r resb 5 ;para cualquier valor, maximo el circulo 0F FFFF FFFF FFFF
+    peri_r resb 5 
+    area_r resb 5
     temp_r resb 5
     
+; Seccion de texto propia de asm 8086
 section .text
-    global _start  ; Declaración global del punto de entrada
+    ;ORG 100h facilita al compilador saber que el codigo es ASM 8086 para DOS
+    org 100h
+    ; Declaración global del punto de entrada
+    global _start 
 
 _start:
-    jmp initial_case  ; Salta a la sección que pregunta si desea iniciar
+    ; Salta a la sección que imprime el mensaje de bienvenida
+    jmp initial_case  
 
 initial_case:
-    ; Imprime el mensaje inicial preguntando si quiere iniciar
-    mov ah, 09h  
-    lea dx, [initial_msg]  
-    int 21h  
+    ; Imprime el mensaje inicial de bienvenida
+    mov bx, initial_msg
+    call imp_msg
     jmp case_show_figures
 
 case_show_figures:
@@ -101,37 +118,44 @@ case_show_figures:
     je case_diamond
     cmp al, '5'
     je case_pentagon
+
+    ;si el digito ingresado no es válido vuelve a preguntar que figura quiere calcular
     jmp case_show_figures 
 
 
 default_case:
     ; Mensaje por defecto en caso de entrada inválida
-    mov ah, 09h  ; Función de MS-DOS para imprimir cadena
-    lea dx, [default_ms]  ; Carga la dirección del mensaje por defecto en dx
-    int 21h  ; Interrupción de MS-DOS para imprimir cadena
-    jmp initial_case  ; Salta a terminar el programa
+    mov bx, default_ms
+    call imp_msg
+    ; Salta a imprimir el listado de figuras
+    jmp case_show_figures
 
 repetir:
-    ; Mensaje por defecto en caso de entrada inválida
-    lea dx, [repetir_msg]  
-    mov ah, 09h  
-    int 21h 
-    ; Imprime las opciones de figuras disponibles
-    lea dx, [repsi]
-    int 21h 
-
-    lea dx, [repno]  
-    int 21h 
+    ; Mensaje pora preguntar si quiere repetir o no
+    mob bx, repetir_msg
+    call imp_msg
     
+    mob bx, repsi
+    call imp_msg
+
+    mob bx, repno
+    call imp_msg
+    
+    ;comprueba la entrada del usuario (1 o 2)
+    ;1 para continuar con otro calculo
+    ;2 para salir
     mov ah, 01h  
     int 21h 
     cmp al, '1'  
     je case_show_figures  
     cmp al, '2'  
     je done 
+
+    ; en caso de una entrada diferente se vuelve a preguntar si desea repetir
     jmp repetir
 
 reinciar_buf:
+    ; Este metodo se llama para limpiar todo los buffers disponibles en una sola llamada
     mov si, buffer_text+1
     mov cx, 9
     call clear_loop
@@ -173,18 +197,25 @@ reinciar_buf:
     mov si, respuesta
     mov cx, 2
     call clear_loop
-
-    xor al, al
-
+    
+    ;retorno
     ret
 
 clear_loop:
+    ; Entradas: Recibe en si el valor de la dirección del buffer que se desea limpiar, además en CX recibe el tamaño del buffer
+    ; Salidas: Limpia el buffer indicado y coloca 0 en todos sus registros
+    ; Restricciones: Se necesita especificar tanto la dirección del buffer como su tamaño. Puede limpiar buffers de tamaño n
+    
+    ; Escribir 0 en la posición actual del buffer
     xor al, al
-    mov [si], al            ; Escribir 0 en la posición actual del buffer
-    inc si                  ; Avanzar al siguiente byte en el buffer
-    loop clear_loop         ; Repetir hasta que CX sea 0
-
-    ret                     ; Retornar de la subrutina
+    mov [si], al            
+    
+    ; Incrementa si para avanzar al siguiente byte en el buffer
+    inc si    
+    ; Se repite hasta que CX sea 0              
+    loop clear_loop         
+    ; Retorna
+    ret
 
 
 done:
