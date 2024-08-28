e8678b9e740263ecce41cc0af5411f1cab74e44c
Alexander Montero V <alexmv223@pm.me>
2024-08-28 02:54:51 -0600

    [LICENSE] MIT

--- ASM/program.asm
+++ ASM/program.asm
@@ -1,3 +1,11 @@
+; MIT License
+; Copyright (c) 2024 José Bernardo Barquero Bonilla,
+;                    Jose Eduardo Campos Salazar,
+;                    Jimmy Feng Feng,
+;                    Alexander Montero Vargas
+; Consulta el archivo LICENSE para más detalles.
+
+
 [bits 16]
 
 ;Seccion de datos predefinidos
