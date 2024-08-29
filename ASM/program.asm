d35911cfc704297d2e74e85c4be637e40afc131a
joscCE <j.campos@estudiantec.cr>
2024-08-28 18:49:28 -0600

    gui_lita

--- ASM/program.asm
+++ ASM/program.asm
@@ -1,283 +1,155 @@
-[bits 16]
+org 100h
 
-;Seccion de datos predefinidos
-section .data
-    ;Mensaje general de pruebas
-    default_ms db 'Otras cosas', 0x0D, 0x0A, '$' 
+%include "program.asm"  ; Incluir el archivo program.asm
 
-    ;Mensaje de bienvenida
-    initial_msg db 'Bienvenido a GeometryTEC', 0x0A, '$'
+jmp start
+
+start:
+    mov word [current_menu], 1
+    jmp print_menu
+
+print_menu:
+    call clear_screen
+    call print_title
+    mov ax, [current_menu]
+    cmp ax, 1
+    je print_menu_1
+    cmp ax, 2
+    je print_menu_2
+    cmp ax, 3
+    je print_menu_3
+
+input:
+    mov dx, msg4
+    mov ah, 9
+    int 21h
+    mov ah, 0
+    int 16h  ; Espera a que se presione una tecla
     
-    ;Para pregunta se seleccion de figura
-    askn_msg db 0x0A, 'Escoge una figura:', '$' 
-    square db 0x0A, '1. Cuadrado', '$'  
-    circle db 0x0A, '2. Circulo', '$'  
-    triangle db 0x0A, '3. Triangulo','$'  
-    diamond db 0x0A, '4. Diamante', '$'  
-    pentagon db 0x0A, '5. Pentagono', '$'  
-    hexagon db 0x0A, '6. Hexagono', '$'  
-    trapeze db 0x0A, '7. Trapecio','$'  
-    parallelogram db 0x0A, '8. Paralelogramo','$' 
-    rectangle db 0x0A, '9. Rectangulo','$' 
-     exit_msg db 0x0A, '0. Cerrar programa',0x0A,'$'
-
-    ;Pregunta de lados
-    ask_lado db 0x0A, 'Cuanto mide el lado?',0x0A, '$' 
-    ask_l1 db 0x0A, 'Cuanto mide el lado 1?',0x0A, '$'
-    ask_l2 db 0x0A, 'Cuanto mide el lado 2?',0x0A, '$'
-    ask_h db 0x0A, 'Cuanto mide la altura?',0x0A, '$'
+    cmp al, 0Dh ; Verifica si es Enter (0Dh es el código ASCII de Enter)
+    je handle_buttonclick ; Si es Enter, manejarlo
     
-    ;Respuestas de perimetro y área
-    result_peri db 0x0A, 'El perimetro es: ', '$'
-    result_area db 0x0A, 'El area es: ', '$'
+    cmp al, 1Bh ; Verifica si es Esc (1Bh es el código ASCII de Escape)
+    je exit ; Si es Esc, salir del programa
 
-    ; Mensaje de para consultar si quiere hacer otro calculo
-    repetir_msg db 0x0A, 'Por favor presione:', '$'
-    repsi db 0x0A,'1. Hacer otra operacion','$' 
-    repno db 0x0A,'2. Salir',0x0A,'$'
-
-    ;Mensaje de agradecimiento
-    salida_msg db 0x0A, 'Gracias por usar GeometryTEC:','$'  ; Mensaje de salida
-
-;Seccion de buffer usados en todo el programa para el almacenamiento de valores y datos
-section .bss   
-    ;buffer general de entrada de texto (reutilizable)
-    buffer_text resb 10 ; maxima entrada del input (7 máx y 2 bytes de control)
+    cmp al, 0 ; Verifica si es una tecla especial (tecla de flecha)
+    je handle_special_key ; Si es una tecla especial, manejarla
     
-    ;buffer para datos numericos (hasta 3 para las figuras que lo ocupan)
-    dato_01 resb 3 ;dato Numerico para input 1
-    dato_02 resb 3 ;dato Numerico para input 2
-    dato_03 resb 3 ;dato Numerico para input 3
-    dato_04 resb 3 ;dato Numerico para input 4
-    dato_05 resb 3 ;dato Numerico para input 5
+    jmp input ; Si no es una tecla reconocida, esperar de nuevo
+
+handle_special_key:
+    mov ah, 0 ; Captura la siguiente parte del código de escaneo
+    int 16h
     
-    ;Buffer a usar en operaciones matematicas (guardar direcciones de los buffers)
-    operando1 resb 2
-    operando2 resb 2
-    respuesta resb 2
+    ; Manejo de teclas de flecha
+    cmp ah, 50h ; Flecha hacia abajo
+    je down_pressed
+    cmp ah, 48h ; Flecha hacia arriba
+    je up_pressed
+    jmp input ; Si no es una tecla reconocida, volver al menú
+ 
+inc_menu:
+    inc word [current_menu] ; Incrementa el menú actual
+    cmp word [current_menu], 4 ; Si es mayor a 3, resetear a 1
+    jb print_menu
+    mov word [current_menu], 1
+    jmp print_menu
+ 
+dec_menu:
+    dec word [current_menu] ; Decrementa el menú actual
+    cmp word [current_menu], 0 ; Si es menor a 1, resetear a 3
+    ja print_menu
+    mov word [current_menu], 3
+    jmp print_menu
+ 
+up_pressed:
+    jmp dec_menu
+ 
+down_pressed:
+    jmp inc_menu
+ 
+print_menu_1:
+    mov dx, M1
+    mov ah, 9
+    int 21h
+    jmp input
+ 
+print_menu_2:
+    mov dx, M2
+    mov ah, 9
+    int 21h
+    jmp input
+ 
+print_menu_3:
+    mov dx, M3
+    mov ah, 9
+    int 21h
+    jmp input
+ 
+handle_buttonclick:
+    mov ax, [current_menu]
+    cmp ax, 1
+    je b1c
+    cmp ax, 2
+    je b2c
+    cmp ax, 3
+    je b3c
 
-    ;Buffer numerico de 3 bytes multiproposito
-    buftemp resb 3
-    buftemp2 resb 3
-    buftemp3 resb 3
+b1c:
+    call clear_screen
+    call print_title
+    call case_show_figures  ; Llama a la función case_show_figures
+    jmp print_menu
 
-    ;Buffers para respuestas, de 5 bytes
-    ; 4 bytes max para la parte entera
-    ; 1 byte para la parte decimal
-    peri_r resb 5 
-    area_r resb 5
-    temp_r resb 5
-    
-; Seccion de texto propia de asm 8086
-section .text
-    ;ORG 100h facilita al compilador saber que el codigo es ASM 8086 para DOS
-    org 100h
-    ; Declaración global del punto de entrada
-    global _start 
+b2c:
+    call clear_screen
+    call print_title
+    call case_show_figures  ; Llama a la función case_show_figures
+    jmp print_menu
 
-_start:
-    ; Salta a la sección que imprime el mensaje de bienvenida
-    jmp initial_case  
+b3c:
+    call clear_screen
+    call print_title
+    call case_show_figures  ; Llama a la función case_show_figures
+    jmp print_menu
 
-initial_case:
-    ; Imprime el mensaje inicial de bienvenida
-    mov bx, initial_msg
-    call imp_msg
-    jmp case_show_figures
+exit:
+    call clear_screen
+    mov ah, 4Ch
+    mov al, 0
+    int 21h
 
-case_show_figures:
-    call reinciar_buf
-    ; Pregunta qué figura quiere el usuario
-    lea dx, [askn_msg]  
-    mov ah, 09h  
-    int 21h 
-    ; Imprime las opciones de figuras disponibles
-    lea dx, [square]
-    int 21h 
-
-    lea dx, [circle]  
-    int 21h 
-
-    lea dx, [triangle]  
-    int 21h  
-
-    lea dx, [diamond]  
-    int 21h  
-
-    lea dx, [pentagon] 
-    int 21h  
-
-    lea dx, [hexagon]  
-    int 21h  
-
-    lea dx, [trapeze]  
-    int 21h  
-
-    lea dx, [parallelogram]  
-    int 21h  
-    
-    lea dx, [rectangle]  
-    int 21h 
-
-    lea dx, [exit_msg]  
-    int 21h  
-
-
-    mov ah, 01h  
-    int 21h 
-    cmp al, '1'  
-    je case_square  
-    cmp al, '2'  
-    je case_circle 
-    cmp al, '3'
-    je case_triangle
-    cmp al, '4'
-    je case_diamond
-    cmp al, '5'
-    je case_pentagon
-    cmp al, '6'
-    je case_hexagon
-    cmp al, '7'
-    je case_trapeze
-    cmp al, '8'
-    je case_parallelogram
-    cmp al, '9'
-    je case_rectangle
-    cmp al, '0'
-    je done
-
-
-    ;si el digito ingresado no es válido vuelve a preguntar que figura quiere calcular
-    jmp case_show_figures 
-
-
-default_case:
-    ; Mensaje por defecto en caso de entrada inválida
-    mov bx, default_ms
-    call imp_msg
-    ; Salta a imprimir el listado de figuras
-    jmp case_show_figures
-
-repetir:
-    ; Mensaje pora preguntar si quiere repetir o no
-    mov bx, repetir_msg
-    call imp_msg
-    
-    mov bx, repsi
-    call imp_msg
-
-    mov bx, repno
-    call imp_msg
-    
-    ;comprueba la entrada del usuario (1 o 2)
-    ;1 para continuar con otro calculo
-    ;2 para salir
-    mov ah, 01h  
-    int 21h 
-    cmp al, '1'  
-    je case_show_figures  
-    cmp al, '2'  
-    je done 
-
-    ; en caso de una entrada diferente se vuelve a preguntar si desea repetir
-    jmp repetir
-
-reinciar_buf:
-    ; Este metodo se llama para limpiar todo los buffers disponibles en una sola llamada
-    mov si, buffer_text+1
-    mov cx, 9
-    call clear_loop
-    
-    mov si, dato_01
-    mov cx, 3
-    call clear_loop
-    
-    mov si, dato_02
-    mov cx, 3
-    call clear_loop
-
-    mov si, dato_03
-    mov cx, 3
-    call clear_loop
-
-    mov si, dato_04
-    mov cx, 3
-    call clear_loop
-
-    mov si, dato_05
-    mov cx, 3
-    call clear_loop
-    
-    mov si, buftemp
-    mov cx, 3
-    call clear_loop
-
-    mov si, buftemp2
-    mov cx, 3
-    call clear_loop
-
-    mov si, buftemp3
-    mov cx, 3
-    call clear_loop
-
-    mov si, peri_r
-    mov cx, 5
-    call clear_loop
-
-    mov si, area_r
-    mov cx, 5
-    call clear_loop
-
-    mov si, temp_r
-    mov cx, 5
-    call clear_loop
-
-    mov si, operando1
-    mov cx, 2
-    call clear_loop
-    mov si, operando2
-    mov cx, 2
-    call clear_loop
-    mov si, respuesta
-    mov cx, 2
-    call clear_loop
-    
-    ;retorno
+clear_screen:
+    mov ah, 0
+    mov al, 3
+    int 10h        
     ret
 
-clear_loop:
-    ; Entradas: Recibe en si el valor de la dirección del buffer que se desea limpiar, además en CX recibe el tamaño del buffer
-    ; Salidas: Limpia el buffer indicado y coloca 0 en todos sus registros
-    ; Restricciones: Se necesita especificar tanto la dirección del buffer como su tamaño. Puede limpiar buffers de tamaño n
-    
-    ; Escribir 0 en la posición actual del buffer
-    xor al, al
-    mov [si], al            
-    
-    ; Incrementa si para avanzar al siguiente byte en el buffer
-    inc si    
-    ; Se repite hasta que CX sea 0              
-    loop clear_loop         
-    ; Retorna
+print_title:
+    mov dx, title
+    mov ah, 9
+    int 21h
     ret
 
+msg3: db 'You clicked button 3.', 0ah, 0dh, '$'
+msg2: db 'You clicked button 2.', 0ah, 0dh, '$'
+msg1: db 'You clicked button 1.', 0ah, 0dh, '$'
+msg4: db 'Use arrow keys to navigate, Enter to select, Esc to exit.', 0ah, 0dh, '$'
 
-done:
-    lea dx, [salida_msg]  
-    mov ah, 09h  
-    int 21h 
-    mov ah, 4Ch  ; Función de MS-DOS para terminar el programa
-    int 21h  ; Interrupción de MS-DOS para salir
+title: 
+    db 'geotec', 0ah, 0dh, '$'
 
+current_menu dw 1
 
-%include 'io.inc'
-%include 'calc.inc'
-%include 'figuras/square.inc'
-%include 'figuras/circle.inc'
-%include 'figuras/diamond.inc'
-%include 'figuras/pentagon.inc'
-%include 'figuras/trapeze.inc'
-%include 'figuras/rectangle.inc'
-%include 'figuras/parallelogram.inc'
-%include 'figuras/hexagon.inc'
-%include 'figuras/triangle.inc'
+M1 db '  ---------------  ', 0ah, 0dh
+   db '  | >Figuras   |   ', 0ah, 0dh
+   db '  ---------------  ', 0ah, 0dh, '$'
+
+M2 db '  ---------------  ', 0ah, 0dh
+   db '  | >Option 2  |   ', 0ah, 0dh
+   db '  ---------------  ', 0ah, 0dh, '$'
+
+M3 db '  ---------------  ', 0ah, 0dh
+   db '  | >Salir     |   ', 0ah, 0dh
+   db '  ---------------  ', 0ah, 0dh, '$'
+ret
